927ea4fddcade2b3f5eff2f94b06d9ff7c13bf74
Kaisa Lumiste <lumistekaisa@gmail.com>
2024-09-15 16:37:02 +0300

    week5

---
+++ Week5AndOrLogicalOperators/Program.cs
@@ -0,0 +1,43 @@
+﻿//AND (&&) and OR (||)logical operators
+
+//AND
+//true && true => true
+//true && false => false
+//false && true => false
+//false && false => false
+
+string username, password; //username: admin //password: admin1234
+
+Console.WriteLine("Enter username:");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter password:");
+password = Console.ReadLine();
+
+//OR
+//true || true => true
+//true || false => true
+//false || true => true
+//false || false => false
+
+if(username != "admin" || password != "admin1234") //!= these symbols together means NOT
+{
+    Console.WriteLine("Invalid user credentials.");
+}
+else
+{
+    Console.WriteLine("Welcome!");
+}
+
+
+
+
+
+//if (username == "admin" && password == "admin1234")
+//{
+//    Console.WriteLine("Welcome!");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid user credentials.");
+//}
